f1c4454779c4cc42b28acb469e68a7f581f5ecad
Yanhe <yanhe.liu@cs.helsinki.fi>
2015-07-06 15:06:11 +0300

    hashing

---
+++ src/main/com/daa/hash/SeparateChainingHashTable.java
@@ -0,0 +1,163 @@
+package com.daa.hash;
+
+import java.util.List;
+import java.util.LinkedList;
+import java.util.Iterator;
+
+// using linked-list
+public class SeparateChainingHashTable<T> implements Iterable<T> {
+    private static final int DEFAULT_TABLE_SIZE = 101;
+    private List<T> [] lists;
+    private int currentSize;
+
+
+    public SeparateChainingHashTable() {
+        this(DEFAULT_TABLE_SIZE);
+    }
+
+    public SeparateChainingHashTable(int s) {
+        lists = new LinkedList[nextPrime(s)];
+        for (int i = 0; i < lists.length; i++) {
+            lists[i] = new LinkedList<>();
+        }
+        currentSize = 0;
+    }
+
+    public void clear() {
+        for (int i = 0; i < lists.length ; i++) {
+            lists[i].clear();
+        }
+        currentSize = 0;
+    }
+
+    public int size() {
+        return currentSize;
+    }
+
+    public boolean contains(T x) {
+        List<T> hashedList = lists[hash(x)];
+        return hashedList.contains(x);
+    }
+
+    public void insert(T x) {
+        List<T> hashedList = lists[hash(x)];
+        if (!hashedList.contains(x)) {
+            hashedList.add(x);
+
+            if (++currentSize > lists.length) {
+                rehash();
+            }
+        }   
+    }
+
+    public void remove(T x) {
+        List<T> hashedList = lists[hash(x)];
+        if (hashedList.contains(x)) {
+            hashedList.remove(x);
+            currentSize--;
+        }
+    }
+
+    private int hash(T x) {
+        int val = x.hashCode(); // java inherent method
+
+        val %= lists.length;
+        if (val < 0) {
+            val += lists.length;
+        }
+
+        return val;
+    }
+
+    private static int nextPrime(int n) {
+        if (n % 2 == 0) {
+            n++;
+        }
+
+        while (!isPrime(n)) {
+            n += 2;
+        }
+
+        return n;
+    }
+
+    private static boolean isPrime(int n) {
+        if (n == 2 || n == 3) {
+            return true;
+        }
+
+        if (n == 1 || n % 2 == 0) {
+            return false;
+        }
+
+        for (int i = 3; i * i <= n ; i += 2 ) {
+            if (n % i == 0) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    private void rehash() {
+        List<T>[] oldLists = lists; 
+        lists = new LinkedList[nextPrime(2 * lists.length)];
+        for (int i = 0; i < lists.length; i++) {
+            lists[i] = new LinkedList<>();
+        }
+
+        currentSize = 0;
+        for(int i = 0; i < oldLists.length; i++) {
+            for (T item : oldLists[i]) {
+                insert(item);
+            }
+        }
+    }
+
+    public Iterator<T> iterator() {
+        return new InternalIterator<T>();
+    }
+
+    private class InternalIterator<T> implements Iterator<T> {
+        int current;
+        int listIndex;
+
+        public InternalIterator() {
+            listIndex = 0;
+            skipNullList();
+        }
+
+        public boolean hasNext() {
+            if (listIndex >= lists.length) {
+                return false;
+            }
+
+            return true;
+        }
+
+        public T next() {
+            T currentItem = ((LinkedList<T>)lists[listIndex]).get(current++);
+            if (current == ((LinkedList<T>)lists[listIndex]).size()) {
+                listIndex++;
+                skipNullList();
+            }
+
+            return currentItem;
+        }
+
+        private void skipNullList() {
+            while(listIndex < lists.length && ((LinkedList<T>)lists[listIndex]).isEmpty()) {
+                listIndex++;
+            }
+
+            if (listIndex != lists.length) {
+                current = 0;
+            }
+        }
+
+        public void remove() {
+            // not defined yet
+        }
+    }
+
+}
